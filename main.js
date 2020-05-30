2cb7857ffba89ccb4c29d120835ea193dbb97ae8
jobbot <joevarela83@gmail.com>
2020-05-30 17:38:45 -0600

    Add button varialbles and refactor changeColor function

--- main.js
+++ main.js
@@ -4,23 +4,33 @@ var userMin = document.querySelector('.min-input');
 var userSec = document.querySelector('.sec-input');
 var startBtn = document.querySelector('.start-activity-btn');
 var activityContainer = document.querySelector('.btn-container');
-
+var studyIcon = document.querySelector('.study-icon');
+var meditateIcon = document.querySelector('.meditate-icon');
+var exerciseIcon = document.querySelector('.exercise-icon');
+var studyBtn = document.querySelector('.study-btn');
+var meditateBtn = document.querySelector('.meditate-btn');
+var exerciseBtn = document.querySelector('.exercise-btn');
 
 
 var activityContainer = document.querySelector('.btn-container');
 
  
 
-
 activityContainer.addEventListener('click', changeColor);
 
 
 function changeColor(event) { 
   if (event.target.className === 'study-btn') {
-    alert('WE DID IT!')
+    studyIcon.src = 'assets/study-active.svg';
+    studyBtn.classList.add('study-btn-active');
   } else if (event.target.className === 'meditate-btn') {
-    alert('HELL YEAH!!!')
+    meditateIcon.src = 'assets/meditate-active.svg';
+    meditateBtn.classList.add('meditate-btn-active');
   } else if (event.target.className === 'exercise-btn') {
-    alert('BOOOOOOM!')
+    exerciseIcon.src = 'assets/exercise-active.svg';
+    exerciseBtn.classList.add('exercise-btn-active');
   }
 }
+// tagline1.innerText = tagline1Input.value;
+// tagline2.innerText = tagline2Input.value;
+// bookTitle.innerText = titleInput.value;
