b1040847f9673d723724cb8e1c6978e48e6d329b
Taryn Martin <taryn.martin919@gmail.com>
2020-06-02 15:34:25 -0600

    Move startTimer function to activity class to be a method

--- activity.js
+++ activity.js
@@ -8,8 +8,21 @@ class Activity {
     this.completed = false;
   }
 
-  startTimer() {
-    // This should start the timer for whatever activity selected
+  startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
     return this.id;
   }
 
